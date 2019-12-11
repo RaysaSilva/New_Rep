d29ec1903abcdcbe7c182dd0991d4797de48d2b2
RaysaSilva <raysaconceicaosilva@gmail.com>
2019-12-11 17:04:31 -0300

    Adicionado desafio_tres branch atualizado

--- King.py
+++ King.py
@@ -1,26 +1,7 @@
-frsone = "Why so serious ?" 
-print ("String 1:",frsone) 
-frstwo = "Run Florest, Run"
-nuone = len(frsone)
-nutwo = len(frstwo)
-t = "Tamanho de '%s' é {}" % (frsone)
-x = "Tamanho de '%s' é {}" % (frstwo)
-print ("String 2:", frstwo) 
-print (t.format (nuone))
-print (x.format(nutwo))
-if (len(frsone) != len(frstwo)):
-    print ("As duas strings são de tamanhos diferentes")
-else :
- print("As duas strings são iguais") 
-
-if frsone == frstwo:
-    print('Iguais')
-else:
-    print('Diferente')
-if (x == True):
-    print("As duas strings possuem conteúdo iguais")
-else:
-    print("As duas strings possuem conteúdo diferente")
+nome = str(input('Digite seu nome completo :'))
+for letras in nome:
+    letras_final = letras.upper()
+    print(letras_final)
 
 
 
