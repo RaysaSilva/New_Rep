5b41c4ed79633a515065c3f4001da249199d8cab
RaysaSilva <raysaconceicaosilva@gmail.com>
2019-12-11 11:56:13 -0300

    deletei de novo .vscode/

--- King.py
+++ King.py
@@ -5,12 +5,15 @@ print('New branch')
 frase = 'Ola, eu sou uma frase. Me quebre em palavras e conte quantas palavras eu contenho'
 lista_de_palavras = frase.split(' ')
 nova_palavra = ''
-lista = ['TESTE']
+lista = []
 for palavras in lista_de_palavras:
     for letras in palavras:
         if letras in ascii_letters:
             nova_palavra = nova_palavra + letras
-            if nova_palavra in lista:
-                lista.append(nova_palavra)
-                print(lista)
-                print(len(lista))
+    if nova_palavra not in lista:
+        lista.append(nova_palavra)
+        nova_palavra = ''
+        
+print(lista)
+print(len(lista))
+                
