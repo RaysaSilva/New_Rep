b2e1371c6fac09247c61cd5e941cd4d79686b8cc
RaysaSilva <raysaconceicaosilva@gmail.com>
2019-12-11 11:39:17 -0300

    modificado o King.py e adicionei .vscode/

--- King.py
+++ King.py
@@ -1,2 +1,16 @@
+from string import ascii_letters
 print("olá")
 print('New branch')
+
+frase = 'Ola, eu sou uma frase. Me quebre em palavras e conte quantas palavras eu contenho'
+lista_de_palavras = frase.split(' ')
+nova_palavra = ''
+lista = ['TESTE']
+for palavras in lista_de_palavras:
+    for letras in palavras:
+        if letras in ascii_letters:
+            nova_palavra = nova_palavra + letras
+            if nova_palavra in lista:
+                lista.append(nova_palavra)
+                print(lista)
+                print(len(lista))
